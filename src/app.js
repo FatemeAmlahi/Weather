2782fe87ba52d4ffdb2ff67192f2b90877d4c164
FatemeAmlahi <101039026+FatemeAmlahi@users.noreply.github.com>
2022-07-14 05:48:06 -0700

    showing the current temprature

--- src/app.js
+++ src/app.js
@@ -1,3 +1,62 @@
+function currentDate() {
+  let date = now.getDate();
+  let hours = now.getHours();
+  let minutes = now.getMinutes();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[now.getDay()];
+  let months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+  let month = months[now.getMonth()];
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  return `${month}${date},${day},${hours}:${minutes}`;
+}
+
+let now = new Date();
+let calendar = document.querySelector("#time");
+calendar.innerHTML = currentDate();
+
+function showTemprature(response) {
+  let temprature = document.querySelector("#temprature");
+  let city = document.querySelector("#city");
+  let description = document.querySelector("#description");
+  let feelsLike = document.querySelector("#feelLike");
+  let humidity = document.querySelector("#humidity");
+  let wind = document.querySelector("#wind");
+
+  temprature.innerHTML = Math.round(response.data.main.temp);
+  city.innerHTML = `${response.data.name},${response.data.sys.country}`;
+  description.innerHTML = response.data.weather[0].description;
+  feelsLike.innerHTML = Math.round(response.data.main.feels_like);
+  humidity.innerHTML = response.data.main.humidity;
+  wind.innerHTML = Math.round(response.data.wind.speed);
+  console.log(response.data);
+}
+
 let apiKey = "d79f110cc683c7f64eae529b0bc53eaf";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=London&appid=${apiKey}&units=metric`;
-console.log(apiUrl);
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Tehran&appid=${apiKey}&units=metric`;
+axios.get(apiUrl).then(showTemprature);
